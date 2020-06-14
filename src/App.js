247ec0ce15c9067edcaf6c202339f585b2740c46
Bushra Hussain <BushraHussain@users.noreply.github.com>
2020-06-14 03:53:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import './App.css';
+
 
 function App() {
   return (
-    <div>
-      <h2> My first react App</h2>
-   
-    Hello from Bushra !! Updated 
-    </div>
+ <h1>Hello from lexus - updated today</h1>
   );
 }
 
